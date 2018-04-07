dbb2a7ba2d895413aee1af97eef9501283c90def
James Chou <james.chou.career@gmail.com>
2018-04-07 14:49:15 -0400

    root route added and corrected feedback validation typo

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
 
+  root to:'businesses#index'
   namespace :api do
     namespace :v1 do
       get '/businesses', to: 'businesses#index'
     end
   end
-  
+
 end
