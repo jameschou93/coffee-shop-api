7d69ead658c856257bb0dc95e080ca4d2add10b2
James Chou <james.chou.career@gmail.com>
2018-04-16 18:48:45 -0400

    added routes for businesses

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to:'businesses#index'
   namespace :api do
     namespace :v1 do
-      get '/businesses', to: 'businesses#index'
+      resources :businesses
     end
   end
 
