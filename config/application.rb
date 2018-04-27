68f68b4b3a6a41beb0c0e5a29cd4a3cc5019edfd
James Chou <james.chou.career@gmail.com>
2018-04-27 18:05:13 -0400

    allowed access control temporarily

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,10 @@ module CoffeeShopApi
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
+    config.action_dispatch.default_headers.merge!({
+      'Access-Control-Allow-Origin' => '*',
+      'Access-Control-Request-Method' => '*'
+    })
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     config.middleware.use ActionDispatch::Flash
